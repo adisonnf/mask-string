8303bd5273023563535df0498b966e08cf29e0ea
adisonnf <177836378+adisonnf@users.noreply.github.com>
2025-07-21 19:06:10 +0500

    Create mask.php

---
+++ mask.php
@@ -0,0 +1,16 @@
+<?php
+function mask($stringToMask)
+{
+    $length = strlen($stringToMask);
+    if ($length <= 4) {
+        return $stringToMask;
+    }
+
+    $maskedPart = str_repeat('#', $length - 4);
+    $visiblePart = substr($stringToMask, -4);
+    return $maskedPart . $visiblePart;
+}
+
+#вывод в консоли
+$stringToMask = trim(fgets(STDIN));
+echo mask($stringToMask) . PHP_EOL;
